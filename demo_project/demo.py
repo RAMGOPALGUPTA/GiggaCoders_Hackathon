709d66dc74ef9015042d7a7e2f221510b8cf9bbc
Amay-droid <amaybhardwaj262@gmail.com>
2026-08-30 13:22:42 +0530

    Add demo project for Sentinel demonstration

---
+++ demo_project/demo.py
@@ -0,0 +1,10 @@
+#Fake creditionals
+password = "ModifiedPassword456!"
+
+api_key = "sk_test_1234567890abcdef"
+
+placeholder_password = "YOUR_PASSWORD_HERE"
+
+placeholder_api_key = "YOUR_API_KEY_HERE"
+
+message = "Original project content."
